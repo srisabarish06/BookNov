2e3e4ea4aeeaacead64640c40a6c068bb48991e2
srisabarish06 <srisabarish128.com>
2025-11-05 01:30:01 +0530

    Update Hero component content for enhanced messaging and branding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,9 @@ const Hero = () => {
   return (
     <section className="w-full h-screen bg-[url('/src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center flex items-center">
       <div className="max-w-5xl mx-auto px-6 text-white">
-        <h1 className="text-4xl md:text-6xl font-extrabold">Welcome to BookNov</h1>
-        <p className="mt-4 text-lg md:text-xl opacity-90">Find and book the best hotels and experiences.</p>
+        <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
+        <h1 className='font-playfair text-2xl md:test-5xl md:text-[56px] md:leading-[56px] font-bold  md:font-extrabold max-w-x1 mt-4'>Discover Your Perfect Gateway Destination</h1>
+        <p>Unparalleled luxury and comfort awaits at the world's most exclusive hotels and resorts. Start your journey today</p>
       </div>
     </section>
   )
