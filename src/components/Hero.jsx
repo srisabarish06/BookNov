a8ae1d5f8fc1855322d2c39b0ffc8d3f97b3e12d
srisabarish06 <srisabarish128.com>
2025-11-05 01:33:02 +0530

    Update padding in Hero component for improved text visibility

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="w-full h-screen bg-[url('/src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center flex items-center">
       <div className="max-w-5xl mx-auto px-6 text-white">
-        <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
+        <p className='bg-[#49B9FF]/50 px-95 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
         <h1 className='font-playfair text-2xl md:test-5xl md:text-[56px] md:leading-[56px] font-bold  md:font-extrabold max-w-x1 mt-4'>Discover Your Perfect Gateway Destination</h1>
         <p>Unparalleled luxury and comfort awaits at the world's most exclusive hotels and resorts. Start your journey today</p>
       </div>
