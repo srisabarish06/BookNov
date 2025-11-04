59c9eb3d72488a072214064b782c880ea0176695
srisabarish06 <srisabarish128.com>
2025-11-04 23:17:59 +0530

    Refactor Navbar scroll handling to use window scroll event and remove unnecessary scrollable container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,19 @@ const Navbar = () => {
         { name: 'About', path: '/' },
     ];
 
-    const ref = React.useRef(null)
 
     const [isScrolled, setIsScrolled] = React.useState(false);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
     React.useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(ref.current.scrollTop > 10);
+            setIsScrolled(window.scrollY > 10);
         };
-        ref.current.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
     return (
-        <div ref={ref} className="h-88 md:h-64 overflow-y-scroll">
-
             <nav className={`fixed top-0 left-0 bg-indigo-500 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
 
                 {/* Logo */}
@@ -90,7 +87,6 @@ const Navbar = () => {
                     </button>
                 </div>
             </nav>
-        </div>
     );
 }
 
