edf5aa4888924b371943580417a94b254e28b7d5
srisabarish06 <srisabarish128.com>
2025-11-05 00:55:23 +0530

    Add Home and Hero components; update App to include routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
-import { useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -8,6 +9,12 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        
+      </div>
    </div>
   );
 }
