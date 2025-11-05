0d273cd4ce8bf40fc2781c3d0ec98c32603ca950
srisabarish06 <srisabarish128.com>
2025-11-05 07:38:52 +0530

    Add Clerk integration and update environment configuration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { assets, cities } from '../assets/assets.js';
 
 const Hero = () => {
   return (
     <section className="w-full h-screen bg-[url('/src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center flex items-center">
       <div className="max-w-5xl mx-auto px-6 text-white">
-        <p className='bg-[#49B9FF]/60 px-95 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
-        <h1 className='font-playfair text-2xl md:test-5xl md:text-[56px] md:leading-14 font-bold  md:font-extrabold max-w-x1 mt-4'>Discover Your Perfect Gateway Destination</h1>
-        <p>Unparalleled luxury and comfort awaits at the world's most exclusive hotels and resorts. Start your journey today</p>
-        <form className='bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+        <p className='bg-[#49B9FF]/60 px-5 py-1 rounded-full mt-20 inline-block'>The Ultimate Hotel Experience</p>
+        <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-tight font-bold md:font-extrabold max-w-xl mt-4'>Discover Your Perfect Gateway Destination</h1>
+        <p className="mt-3 max-w-2xl">Unparalleled luxury and comfort awaits at the world's most exclusive hotels and resorts. Start your journey today</p>
+        <form className='bg-white text-gray-500 rounded-lg px-6 py-4 flex flex-col md:flex-row items-start md:items-center gap-4 w-full md:w-auto mt-6'>
 
             <div>
                 <div className='flex items-center gap-2'>
                   <img src={assets.calenderIcon} alt="" className='h-4 '/>
                     <label htmlFor="destinationInput">Destination</label>
                 </div>
-                <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+                <input list='destinations' id="destinationInput" type="text" className="rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
                 <datalist id="destinations">
                   {cities.map((city, index) => (
                     <option key={index} value={city} />
@@ -41,10 +42,10 @@ const Hero = () => {
 
             <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
                 <label htmlFor="guests">Guests</label>
-                <input min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+                <input min={1} max={4} id="guests" type="number" className="rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none w-16" placeholder="0" />
             </div>
 
-            <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
+            <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer w-full md:w-auto' >
                 <img src={assets.searchIcon} alt="search" className='h-7' />
                 <span>Search</span>
             </button>
